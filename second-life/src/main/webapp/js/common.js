e51dbfd48109561dc8c7be5b83fd01aa90539253
배동호 <bdh0207@naver.com>
2016-03-15 17:27:14 +0900

    재업로드

--- second-life/src/main/webapp/js/common.js
+++ second-life/src/main/webapp/js/common.js
@@ -1,4 +1,4 @@
-var contextRoot = 'http://localhost:8008/second-life';;
+var contextRoot = 'http://localhost:8008/second-life';
 // 'http://192.168.0.33:8008/second-life';
 // 'http://localhost:8008/second-life';
 
